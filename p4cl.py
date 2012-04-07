18fd3965492ca1203d7f09e086b9f206cbb85fe9
Ed Gauthier <ed@egauthier.net>
2012-04-06 21:54:38 -0400

    Add a usage method.

--- p4cl.py
+++ p4cl.py
@@ -43,6 +43,22 @@ def fileMatchesFilter(f,fileFilters):
 
     return False
 
+def _usage():
+    print """
+    Usage: p4cl.py [-f] <filespec>
+
+    -f          Include files from the changelist in the changelog output. Only
+                files that match the <filespec> will be output. The only
+                exception is if the <filespec> includes *, ?, or ... (in the
+                middle of the path - ... at the end of the path works as
+                expected). 
+
+    <filespec>  A Perforce filespec supporting date/changelist filters. 
+                E.g. //depot/project/...@2,@10
+                E.g. //depot/project/...@2012-04-01,@now
+
+    """
+
 def main():
     includeFiles = False
     if sys.argv[1] == '-f':
